2027681cd5f21664b47514be84d48baec17c1fa1
Jamie Brynes <jamiebrynes@improbable.io>
2019-01-24 11:28:56 +0000

    update build script
    build

--- ci/build.sh
+++ ci/build.sh
@@ -5,56 +5,21 @@ cd "$(dirname "$0")/../"
 
 ci/bootstrap.sh
 .shared-ci/scripts/prepare-unity.sh
+.shared-ci/scripts/prepare-unity-mobile.sh "$(pwd)/logs/PrepareUnityMobile.log"
 
 source .shared-ci/scripts/pinned-tools.sh
 source .shared-ci/scripts/profiling.sh
 
 UNITY_PROJECT_DIR="$(pwd)/workers/unity"
 
-# The asset cache ip cannot be hardcoded and so is stored in an environment variable on the build agent.
-# This is bash shorthand syntax for if-else predicated on the existance of the environment variable 
-# where the else branch assigns an empty string.
-#   i.e. - 
-#   if [ -z ${UNITY_ASSET_CACHE_IP} ]; then
-#       ASSET_CACHE_ARG="-CacheServerIPAddress ${UNITY_ASSET_CACHE_IP}"
-#   else
-#       ASSET_CACHE_ARG=""
-#   fi
-
-ASSET_CACHE_ARG=${UNITY_ASSET_CACHE_IP:+-CacheServerIPAddress "${UNITY_ASSET_CACHE_IP}"}
-
 markStartOfBlock "$0"
 
-markStartOfBlock "Building UnityClient"
+.shared-ci/scripts/build.sh "${UNITY_PROJECT_DIR}" UnityClient cloud mono "$(pwd)/logs/UnityClientBuild.log"
+.shared-ci/scripts/build.sh "${UNITY_PROJECT_DIR}" UnityGameLogic cloud mono "$(pwd)/logs/UnityGameLogicBuild.log"
+.shared-ci/scripts/build.sh "${UNITY_PROJECT_DIR}" AndroidClient local mono "$(pwd)/logs/AndroidClientBuild-mono.log"
 
-pushd "${UNITY_PROJECT_DIR}"
-    dotnet run -p ../../.shared-ci/tools/RunUnity/RunUnity.csproj -- \
-        -projectPath "${UNITY_PROJECT_DIR}" \
-        -batchmode \
-        -quit \
-        -logfile "$(pwd)/../../logs/UnityClientBuild.log" \
-        -executeMethod "Improbable.Gdk.BuildSystem.WorkerBuilder.Build" \
-        ${ASSET_CACHE_ARG} \
-        +buildWorkerTypes "UnityClient" \
-        +buildTarget "cloud"
-popd
-
-markEndOfBlock "Building UnityClient"
-
-markStartOfBlock "Building UnityGameLogic"
-
-pushd "${UNITY_PROJECT_DIR}"
-    dotnet run -p ../../.shared-ci/tools/RunUnity/RunUnity.csproj -- \
-        -projectPath "${UNITY_PROJECT_DIR}" \
-        -batchmode \
-        -quit \
-        -logfile "$(pwd)/../../logs/UnityGameLogicBuild.log" \
-        -executeMethod "Improbable.Gdk.BuildSystem.WorkerBuilder.Build" \
-        ${ASSET_CACHE_ARG} \
-        +buildWorkerTypes "UnityGameLogic" \
-        +buildTarget "cloud"
-popd
-
-markEndOfBlock "Building UnityGameLogic"
+if isMacOS; then
+  .shared-ci/scripts/build.sh "${UNITY_PROJECT_DIR}" iOSClient local il2cpp "$(pwd)/logs/iOSClientBuild.log"
+fi
 
 markEndOfBlock "$0"
