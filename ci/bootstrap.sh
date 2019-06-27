2ffbba71ed033a369bf945bbd3b9ea66e8333036
Paul Balaji <paulbalaji@improbable.io>
2019-06-27 12:08:23 +0100

    update pin + revert bootstrap changes
    Signed-off-by: Paul Balaji <paulbalaji@improbable.io>

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -37,6 +37,6 @@ mkdir "${TARGET_DIRECTORY}"
 pushd "${TARGET_DIRECTORY}"
     git init
     git remote add origin "${CLONE_URI}"
-    git fetch --depth 20 origin feature/worker-wip
+    git fetch --depth 20 origin develop
     git checkout "${PINNED_VERSION}"
 popd
