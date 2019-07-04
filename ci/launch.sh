14fed6954aaaa06f19ff9f98fbd755966a6cc714
Paul Balaji <paulbalaji@improbable.io>
2019-07-04 16:09:53 +0100

    split ios/android build step
    Signed-off-by: Paul Balaji <paulbalaji@improbable.io>

--- ci/launch.sh
+++ ci/launch.sh
@@ -4,7 +4,6 @@ set -e -u -x -o pipefail
 
 cd "$(dirname "$0")/../"
 
-source ".shared-ci/scripts/profiling.sh"
 source ".shared-ci/scripts/pinned-tools.sh"
 
 # Download the artifacts and reconstruct the build/assemblies folder.
@@ -12,12 +11,10 @@ buildkite-agent artifact download "build\assembly\**\*" .
 
 uploadAssembly "${ASSEMBLY_PREFIX}" "${PROJECT_NAME}"
 
-markStartOfBlock "Launching deployments"
+echo "Launching deployments"
 
 spatial cloud launch "${ASSEMBLY_NAME}" cloud_launch.json "${ASSEMBLY_NAME}" --snapshot=snapshots/default.snapshot
 
 CONSOLE_URL="https://console.improbable.io/projects/${PROJECT_NAME}/deployments/${ASSEMBLY_NAME}/overview"
 
 buildkite-agent annotate --style "success" "Deployment URL: ${CONSOLE_URL}"
-
-markEndOfBlock "Launching deployments"
