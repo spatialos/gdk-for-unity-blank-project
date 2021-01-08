10184adf72928480e668d06ddfdea14499c15c4d
Jamie Brynes <jamiebrynes7@gmail.com>
2021-01-08 14:29:17 +0000

    update entity templates

--- workers/unity/Assets/BlankProject/Scripts/Config/EntityTemplates.cs
+++ workers/unity/Assets/BlankProject/Scripts/Config/EntityTemplates.cs
@@ -1,41 +1,60 @@
+using System.Collections.Generic;
 using Improbable;
 using Improbable.Gdk.Core;
 using Improbable.Gdk.PlayerLifecycle;
 using Improbable.Gdk.QueryBasedInterest;
+using Improbable.Generated;
 using UnityEngine;
 
 namespace BlankProject.Scripts.Config
 {
     public static class EntityTemplates
     {
-        public static EntityTemplate CreatePlayerEntityTemplate(EntityId entityId, string workerId, byte[] serializedArguments)
+        public static EntityTemplate CreatePlayerEntityTemplate(EntityId entityId, EntityId clientWorkerEntityId,
+            byte[] serializedArguments)
         {
-            var clientAttribute = EntityTemplate.GetWorkerAccessAttribute(workerId);
-            var serverAttribute = UnityGameLogicConnector.WorkerType;
-
-            var position = new Vector3(0, 1f, 0);
-            var coords = Coordinates.FromUnityVector(position);
-
-            var template = new EntityTemplate();
-            template.AddComponent(new Position.Snapshot(coords), clientAttribute);
-            template.AddComponent(new Metadata.Snapshot("Player"), serverAttribute);
-
-            PlayerLifecycleHelper.AddPlayerLifecycleComponents(template, workerId, serverAttribute);
-
-            const int serverRadius = 500;
-            var clientRadius = workerId.Contains(MobileClientWorkerConnector.WorkerType) ? 100 : 500;
-
-            var serverQuery = InterestQuery.Query(Constraint.RelativeCylinder(serverRadius));
-            var clientQuery = InterestQuery.Query(Constraint.RelativeCylinder(clientRadius));
+            var template = BaseTemplate();
+            template.AddComponent(new Position.Snapshot(Coordinates.FromUnityVector(new Vector3(0, 1f, 0))));
+            template.AddComponent(new Metadata.Snapshot("Player"));
+            template.AddPlayerLifecycleComponents(clientWorkerEntityId);
 
             var interest = InterestTemplate.Create()
-                .AddQueries<Metadata.Component>(serverQuery)
-                .AddQueries<Position.Component>(clientQuery);
-            template.AddComponent(interest.ToSnapshot(), serverAttribute);
+                .AddQueries(ComponentSets.PlayerClientSet, InterestQuery.Query(Constraint.RelativeCylinder(500)));
+            template.AddComponent(interest.ToSnapshot());
 
-            template.SetReadAccess(UnityClientConnector.WorkerType, MobileClientWorkerConnector.WorkerType, serverAttribute);
-            template.SetComponentWriteAccess(EntityAcl.ComponentId, serverAttribute);
+            return template;
+        }
 
+        public static EntityTemplate CreatePlayerSpawner()
+        {
+            var template = BaseTemplate();
+            template.AddComponent(new Position.Snapshot());
+            template.AddComponent(new Metadata.Snapshot("PlayerCreator"));
+            template.AddComponent(new Persistence.Snapshot());
+            template.AddComponent(new PlayerCreator.Snapshot());
+
+            return template;
+        }
+
+        public static EntityTemplate CreateLoadBalancingPartition()
+        {
+            var template = BaseTemplate();
+            template.AddComponent(new Position.Snapshot());
+            template.AddComponent(new Metadata.Snapshot("LB Partition"));
+
+            var query = InterestQuery.Query(Constraint.Component<Position.Component>());
+            var interest = InterestTemplate.Create().AddQueries(ComponentSets.AuthorityDelegationSet, query);
+            template.AddComponent(interest.ToSnapshot());
+            return template;
+        }
+
+        private static EntityTemplate BaseTemplate()
+        {
+            var template = new EntityTemplate();
+            template.AddComponent(new AuthorityDelegation.Snapshot(new Dictionary<uint, long>
+            {
+                { ComponentSets.AuthorityDelegationSet.ComponentSetId, 1 }
+            }));
             return template;
         }
     }
--- workers/unity/Assets/Editor/SnapshotGenerator/SnapshotGenerator.cs
+++ workers/unity/Assets/Editor/SnapshotGenerator/SnapshotGenerator.cs
@@ -1,18 +1,15 @@
 using System.IO;
-using Improbable;
+using BlankProject.Scripts.Config;
 using Improbable.Gdk.Core;
-using Improbable.Gdk.PlayerLifecycle;
-using Improbable.Gdk.QueryBasedInterest;
 using UnityEditor;
 using UnityEngine;
-
 using Snapshot = Improbable.Gdk.Core.Snapshot;
 
 namespace BlankProject.Editor
 {
     internal static class SnapshotGenerator
     {
-        private static string DefaultSnapshotPath = Path.GetFullPath(
+        private static readonly string DefaultSnapshotPath = Path.GetFullPath(
             Path.Combine(
                 Application.dataPath,
                 "..",
@@ -34,33 +31,9 @@ namespace BlankProject.Editor
         private static Snapshot CreateSnapshot()
         {
             var snapshot = new Snapshot();
-
-            AddPlayerSpawner(snapshot);
+            snapshot.AddEntity(new EntityId(1), EntityTemplates.CreateLoadBalancingPartition());
+            snapshot.AddEntity(EntityTemplates.CreatePlayerSpawner());
             return snapshot;
-        }
-
-        private static void AddPlayerSpawner(Snapshot snapshot)
-        {
-            var serverAttribute = UnityGameLogicConnector.WorkerType;
-
-            var template = new EntityTemplate();
-            template.AddComponent(new Position.Snapshot(), serverAttribute);
-            template.AddComponent(new Metadata.Snapshot("PlayerCreator"), serverAttribute);
-            template.AddComponent(new Persistence.Snapshot(), serverAttribute);
-            template.AddComponent(new PlayerCreator.Snapshot(), serverAttribute);
-
-            var query = InterestQuery.Query(Constraint.RelativeCylinder(500));
-            var interest = InterestTemplate.Create()
-                .AddQueries<Position.Component>(query);
-            template.AddComponent(interest.ToSnapshot(), serverAttribute);
-
-            template.SetReadAccess(
-                UnityClientConnector.WorkerType,
-                UnityGameLogicConnector.WorkerType,
-                MobileClientWorkerConnector.WorkerType);
-            template.SetComponentWriteAccess(EntityAcl.ComponentId, serverAttribute);
-
-            snapshot.AddEntity(template);
         }
     }
 }
